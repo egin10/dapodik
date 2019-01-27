30b60bf36a08226fc2d59d164eda2ef1bff31a43
egin10 <egin066@gmail.com>
2019-01-27 23:59:32 +0800

    tabOne Done

--- func.php
+++ func.php
@@ -44,6 +44,44 @@ class GetData
         $getDesKel = explode("</td>", $arrTabOne[5])[3];
         $desKel = explode('>', $getDesKel)[1];
 
-        print_r($desKel);
+        //Kecamatan/Kota
+        $getKecKot = explode("</td>", $arrTabOne[6])[3];
+        $kecKot = explode('>', $getKecKot)[1];
+
+        //Kabupaten/Kota
+        $getKabKot = explode("</td>", $arrTabOne[7])[3];
+        $kabKot = explode('>', $getKabKot)[1];
+
+        //Provinsi
+        $getProv = explode("</td>", $arrTabOne[8])[3];
+        $provinsi = explode('>', $getProv)[1];
+
+        //Status Sekolah
+        $getStat = explode("</td>", $arrTabOne[9])[3];
+        $statusSek = explode('>', $getStat)[1];
+
+        //Waktu Penyelenggaraan
+        $getWp = explode("</td>", $arrTabOne[11])[3];
+        $waktuPen = explode('>', $getWp)[1];
+
+        //Jenjang Pendidikan
+        $getJp = explode("</td>", $arrTabOne[13])[3];
+        $jenjangPen = explode('>', $getJp)[1];
+
+        $tabOne = [
+            'nama' => $nama,
+            'npsn' => $npsn,
+            'alamat' => $alamat,
+            'kode_pos' => $kodePos,
+            'desa_kelurahan' => $desKel,
+            'kecamatan' => $kecKot,
+            'kabupaten_kota' => $kabKot,
+            'provinsi' => $provinsi,
+            'status' => $statusSek,
+            'waktu' => $waktuPen,
+            'jenjang' => $jenjangPen
+        ];
+
+        print_r($tabOne);
     }
 }
